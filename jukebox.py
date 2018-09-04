0a82876913e3a23f167b4200fdbdba834cee9794
vinniciussouza <vinniciussouza@yahoo.com.br>
2018-09-04 11:07:31 -0300

    Commitando pelo VSCode

--- jukebox.py
+++ jukebox.py
@@ -1,16 +1,21 @@
+import os
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.listview import ListItemButton
 from kivy.core.audio import SoundLoader
-import os
+
+from pygame import mixer  # Load the required library
+
+mixer.init()
+
 
 class Lista(ListItemButton):
     pass
 
 
 class JukeboxWidget(BoxLayout):
-    
+
     lista_musicas = ObjectProperty()
     musica_atual = ObjectProperty()
     arquivos = os.listdir(
@@ -18,28 +23,26 @@ class JukeboxWidget(BoxLayout):
     sound = None
 
     def pegar_nome(self):
-        if self.lista_musicas.adapter.selection:           
-            if self.sound:
-                self.sound.stop()
+        if self.lista_musicas.adapter.selection:
+            mixer.music.stop()
             selection = self.lista_musicas.adapter.selection[0].text
             self.musica_atual.text = selection
-            self.sound = SoundLoader.load('./Musicas/'+selection)
-            if self.sound:
-                self.sound.play()
+            mixer.music.load('./Musicas/'+selection)
+            mixer.music.play()
 
     def atualizar_lista(self):
         self.lista_musicas.adapter.data.extend(self.arquivos)
         self.lista_musicas._trigger_reset_populate()
-        
+
 
 class JukeboxApp(App):
-    
+
     jk = None
-    
+
     def build(self):
         self.jk = JukeboxWidget()
         return self.jk
-    
+
     def on_start(self):
         self.jk.atualizar_lista()
 
